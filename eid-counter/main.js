45fc229882a9787d8067d68c56d2ec5df7943069
Hasnain Ali <hasnain5f7@gmail.com>
2025-04-07 22:05:09 +0500

    Update main.js

--- eid-counter/main.js
+++ eid-counter/main.js
@@ -3,43 +3,29 @@ var day_display = document.getElementById("day");
 var hour_display = document.getElementById("hour");
 var minute_display = document.getElementById("minute");
 var second_display = document.getElementById("second");
-var main_display = document.getElementById('main');
+var main_display = document.getElementById("main");
 
 function getValue() {
-    var currentTime = new Date().getTime();
-    var eidTime = new Date('7 june 2025').getTime();
+    var now = moment();
+    var eid = moment('2025-06-07');
 
-    var diff = eidTime - currentTime;
+    var duration = moment.duration(eid.diff(now));
 
-    var month = Math.floor(diff / (1000 * 60 * 60 * 24 * 30.44))
-    var day = Math.ceil(diff / (1000 * 60 * 60 * 24) % 30)
-    var hours = Math.floor(diff / (1000 * 60 * 60) % 24);
-    var minute = Math.ceil(diff / (1000 * 60 ) % 60);
-    var second = Math.ceil(diff / (1000) % 60)
-
-    // // console.log(currentTime);
-    // // console.log(eidTime);
-    // console.log(diff);
-    // console.log(month);
-    // console.log(day);
-    // console.log(hours);
-    // console.log(minute);
-    // console.log(second);
-    
-    if (diff < 1) {
-        main.innerHTML = '<h1 id="logo">Eid-ul-fitar</h1><br><h3>Mubarrak!</h3>'
+    if (duration.asMilliseconds() <= 0) {
+        main_display.innerHTML = '<h1 id="logo">Eid-ul-Fitr</h1><br><h3>Mubarak!</h3>';
     } else {
-        month_display.innerText = month;
-        day_display.innerText = day;
+        var months = Math.floor(duration.asMonths());
+        var days = duration.days();
+        var hours = duration.hours();
+        var minutes = duration.minutes();
+        var seconds = duration.seconds();
+
+        month_display.innerText = months;
+        day_display.innerText = days;
         hour_display.innerText = hours;
-        minute_display.innerText = minute;
-        second_display.innerText = second;
+        minute_display.innerText = minutes;
+        second_display.innerText = seconds;
     }
-    
 }
 
-setInterval(()=> {
-    getValue()
-}, 1000)
-
-
+setInterval(getValue, 1000);
